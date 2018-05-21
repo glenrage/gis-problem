ffaa7b5f0de08c7809cd03a1af81ed9217dc5f5b
glenrage <phamgalang@gmail.com>
2018-05-21 15:15:45 -0700

    css changes front end

--- src/components/OutputView.js
+++ src/components/OutputView.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';
+import { Container, Grid } from 'semantic-ui-react';
 
 export default () => {
   return (
     <div className="output-view">
       <div className="output-view--word-list">
         <h3>Word List</h3>
+        <p>Lorem sfkljsdfkj s klfjskljdf sjkdlf kljsdf kjsdf kljsdfjklsdf</p>
       </div>
+
       <div className="output-view--inputstring">
         <h3>Input String</h3>
+        <p>InputString</p>
       </div>
+
       <div className="output-view--output">
-        <h1>Output</h1>
+        <h3>Output</h3>
+        <p>Results</p>
       </div>
     </div>
   );
